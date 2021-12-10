11fae12aada259d528edfc825183e3b1ee9f4108
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-10 12:31:45 +0100

    endopoint fixed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import listendpoints from 'express-list-endpoints'
 import cors from 'cors'
+import mediaRouter from './src/services/index.js'
 
 
 const server = express()
@@ -9,7 +10,7 @@ const port = process.env.PORT || 3001
 server.use(express.json())
 server.use(cors())
 
-
+server.use("/media", mediaRouter)
 
 console.table(listendpoints)
 server.listen(port, () => { console.log(`server is running on ${port}`) })
---
+++ src/services/index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+
+
+const mediaRouter = express.Router()
+
+mediaRouter.get("/", (req, res, next) => {
+    res.send("it works")
+})
+
+
+mediaRouter.get("/:id", (req, res, next) => {
+    res.send("it works 2")
+})
+
+mediaRouter.post("/", (req, res, next) => {
+    res.send("it works 3")
+})
+
+
+mediaRouter.put("/:id", (req, res, next) => {
+    res.send("it works")
+})
+
+mediaRouter.delete("/:id", (req, res, next) => {
+    res.send("it works 4")
+})
+
+mediaRouter.post("/:id/poster", (req, res, next) => {
+    res.send("it works 5")
+})
+
+mediaRouter.post("/:id/reviews", (req, res, next) => {
+    res.send("it works 6")
+})
+
+mediaRouter.delete("/:id/reviews", (req, res, next) => {
+    res.send("it works 7")
+})
+
+
+mediaRouter.get("/:id/pdf", (req, res, next) => {
+    res.send("it works 8")
+})
+
+export default mediaRouter
